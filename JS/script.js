30293c9bb47c921930294d5e8ebd3e2b3ca399ed
Juan Pablo <jpcorvo219@gmail.com>
2024-05-23 14:56:24 -0300

    Format to check
    - added buttons to HTML
    - added a possible metar skeleton to JS

--- JS/script.js
+++ JS/script.js
@@ -24,14 +24,51 @@ function mostrarMetar(info) {
     let intro = `<div>${icaoCode.toUpperCase()} &nbsp ${info.time.repr}</div>`;
     let outro = `<div>${info.temperature.repr}/${info.dewpoint.repr}&nbsp${info.altimeter.repr}&nbsp${info.remarks}</div>`;
 
-    contenedor.innerHTML = intro;
+    contenedor.innerHTML = `
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#codeModal">
+        ${icaoCode.toUpperCase()}
+        </button>
+        <div class="modal fade" id="codeModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal-dialog">
+                <div class="modal-content">    
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-title">${icaoCode.toUpperCase()}</h5> 
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="fs-5">
+                            <p><strong>Designación de OACI para cada aeropuerto</strong><br>Las primeras dos letras indican el país donde se ubica y las últimas dos letras indican el aeropuerto al que corresponde.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#timeModal">
+        ${info.time.repr}
+        </button>
+        <div class="modal fade" id="timeModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal-dialog">
+                <div class="modal-content">    
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-title">${info.time.repr}</h5> 
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="fs-5">
+                            <p>Scattered: Si hay entre 3 y 4 octas de nubes en el cielo se utiliza SCT, significa nubes dispersas</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
 
     if (info.wind_gust) {
         contenedor.innerHTML +=
-            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#windDirModal">
             ${info.wind_direction.repr}
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="windDirModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -41,16 +78,15 @@ function mostrarMetar(info) {
                         <div class="modal-body">
                             <div class="fs-5">
                                 <p>Scattered: Si hay entre 3 y 4 octas de nubes en el cielo se utiliza SCT, significa nubes dispersas</p>
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#windSpeedModal">
             ${info.wind_speed.repr}
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="windSpeedModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -66,10 +102,10 @@ function mostrarMetar(info) {
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#gustModal">
             G
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="gustModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -85,10 +121,10 @@ function mostrarMetar(info) {
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#windGustModal">
             ${info.wind_gust.repr}
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="windGustModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -107,10 +143,10 @@ function mostrarMetar(info) {
 
     } else {
         contenedor.innerHTML +=
-            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#windDirModal">
             ${info.wind_direction.repr} 
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="windDirModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -126,10 +162,10 @@ function mostrarMetar(info) {
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#windSpeedModal">
             ${info.wind_speed.repr} KT
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="windSpeedModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -149,10 +185,10 @@ function mostrarMetar(info) {
     }
     info.wind_variable_direction.forEach(function (variable) {
         contenedor.innerHTML +=
-            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#variableModal">
             ${variable.repr}
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="variableModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -173,10 +209,10 @@ function mostrarMetar(info) {
 
     if (visibility.length < 4) {
         contenedor.innerHTML +=
-            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#visibilityModal">
             ${info.visibility.repr} SM
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="visibilityModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -195,10 +231,10 @@ function mostrarMetar(info) {
 
     } else {
         contenedor.innerHTML +=
-        `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#visibilityModal">
         ${info.visibility.repr}
         </button>
-        <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+        <div class="modal fade" id="visibilityModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
             <div class="modal-dialog">
                 <div class="modal-content">    
                     <div class="modal-header">
@@ -219,10 +255,10 @@ function mostrarMetar(info) {
 
     info.wx_codes.forEach(function (weather) {
         contenedor.innerHTML +=
-            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+            `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#weatherModal">
             ${weather.repr}
             </button>
-            <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+            <div class="modal fade" id="weatherModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                 <div class="modal-dialog">
                     <div class="modal-content">    
                         <div class="modal-header">
@@ -244,10 +280,10 @@ function mostrarMetar(info) {
         let nubeAlt = nube.altitude
         if (nubeAlt.toString().length === 1) {
             contenedor.innerHTML +=
-                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cloudTypeModal">
                 ${nube.type}
                 </button>
-                <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+                <div class="modal fade" id="cloudTypeModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                     <div class="modal-dialog">
                         <div class="modal-content">    
                             <div class="modal-header">
@@ -268,10 +304,10 @@ function mostrarMetar(info) {
                 </div>`
         } else if (nubeAlt.toString().length === 2) {
             contenedor.innerHTML +=
-                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cloudTypeModal">
                 ${nube.type}
                 </button>
-                <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+                <div class="modal fade" id="cloudTypeModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                     <div class="modal-dialog">
                         <div class="modal-content">    
                             <div class="modal-header">
@@ -292,10 +328,10 @@ function mostrarMetar(info) {
                 </div>`
         } else {
             contenedor.innerHTML +=
-                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#dataModal">
+                `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cloudTypeModal">
                 ${nube.type}
                 </button>
-                <div class="modal fade" id="dataModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
+                <div class="modal fade" id="cloudTypeModal" tabindex="-1" aria-labelledby="modal-title" aria-hidden= "true">
                     <div class="modal-dialog">
                         <div class="modal-content">    
                             <div class="modal-header">
@@ -322,6 +358,33 @@ function mostrarMetar(info) {
 
 document.addEventListener("DOMContentLoaded", () => {
     let botonRaw = document.getElementById("botonRaw")
+    let botonCyow = document.getElementById("botonCyow")
+    let botonSumu = document.getElementById("botonSumu")
+    let botonOoms = document.getElementById("botonOoms")
+    let botonKjfk = document.getElementById("botonKjfk")
+    let botonOejn = document.getElementById("botonOejn")
+
+    botonCyow.addEventListener("click", () => {
+        document.getElementById("buscador").value = "CYOW"
+        traerMetar(mostrarMetar)
+    })
+    botonSumu.addEventListener("click", () => {
+        document.getElementById("buscador").value = "SUMU"
+        traerMetar(mostrarMetar)
+    })
+    botonOoms.addEventListener("click", () => {
+        document.getElementById("buscador").value = "OOMS"
+        traerMetar(mostrarMetar)
+    })
+    botonKjfk.addEventListener("click", () => {
+        document.getElementById("buscador").value = "KJFK"
+        traerMetar(mostrarMetar)
+    })
+
+    botonOejn.addEventListener("click", () => {
+        document.getElementById("buscador").value = "OEJN"
+        traerMetar(mostrarMetar)
+    })
 
     botonRaw.addEventListener("click", () => {
         traerMetar(mostrarMetar)
